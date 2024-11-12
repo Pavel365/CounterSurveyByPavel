fb3cd025bc961ebab8dbebe200c116ce95433604
Pavel <pavelzhilkov@abv.bg>
2024-11-12 22:54:10 +0200

    Upload program

---
+++ Counter Survey/Counter Survey/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Counter_Survey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("File.txt");
+
+            Stack<string> strings = new Stack<string>();
+
+            using (reader)
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    strings.Push(line.ToLower());
+                }
+            }
+
+            Dictionary<string, int> teachers = new Dictionary<string, int>();
+
+            while (strings.Count > 0)
+            {
+                CountTeachers(strings.Pop(), teachers);
+            }
+
+
+            StreamWriter writer = new StreamWriter("newFile.txt", false);
+            
+            foreach (var kvp in teachers)
+            {
+                writer.WriteLine($"{kvp.Key} - {kvp.Value}");
+            }
+
+            writer.Close();
+        }
+
+        private static Dictionary<string, int> CountTeachers(string str, Dictionary<string, int> teachers)
+        {
+            if (!teachers.ContainsKey(str))
+            {
+                teachers.Add(str, 0);
+            }
+
+            teachers[str]++;
+
+            return teachers;
+        }
+    }
+}
